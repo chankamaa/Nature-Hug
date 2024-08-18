6b90196ea85a4a226c24be4582b38c175ab1bb89
Peshala G A K J <jayanikalansooriya24@gmail.com>
2024-08-18 17:04:34 +0530

    Fullorder
    frontend
    EmptyCart
    FullCart
    OrderStep01
    OrderStep02
    OrderStep03
    OrderStep04

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmptyCart from './pages/EmptyCart/EmptyCart';
+import FullCart from './pages/FullCart/FullCart';
+import Step01 from './pages/OrderStep01/Step01';
+import Step02 from './pages/OrderStep02/Step02';
+import Step03 from './pages/OrderStep03/Step03';
+import Step04 from './pages/OrderStep04/Step04';
+import Aboutus from './pages/AboutUs/Aboutus';
 
-
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-    
-  )
+    <BrowserRouter>
+      <Routes>
+        {/*Order manager Routes */}
+        <Route path="EmptyCart" element={<EmptyCart />} />
+        <Route path="FullCart" element={<FullCart />} />
+        <Route path="Step01" element={<Step01 />} />
+        <Route path="Step02" element={<Step02 />} />
+        <Route path="Step03" element={<Step03 />} />
+        <Route path="Step04" element={<Step04 />} />
+        <Route path="Aboutus" element={<Aboutus />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
