b607d1135e30a1659f8d455ae5c79356a2374110
Peshala G A K J <jayanikalansooriya24@gmail.com>
2024-08-18 17:28:04 +0530

    App.jsx
    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import EmptyCart from './pages/EmptyCart/EmptyCart';
 import FullCart from './pages/FullCart/FullCart';
 import Step01 from './pages/OrderStep01/Step01';
@@ -8,21 +8,22 @@ import Step03 from './pages/OrderStep03/Step03';
 import Step04 from './pages/OrderStep04/Step04';
 import Aboutus from './pages/AboutUs/Aboutus';
 
-function App() {
+const App=() =>{
   return (
-    <BrowserRouter>
+   <div className='app'>
+<Navbar />
       <Routes>
-        {/*Order manager Routes */}
-        <Route path="EmptyCart" element={<EmptyCart />} />
-        <Route path="FullCart" element={<FullCart />} />
-        <Route path="Step01" element={<Step01 />} />
-        <Route path="Step02" element={<Step02 />} />
-        <Route path="Step03" element={<Step03 />} />
-        <Route path="Step04" element={<Step04 />} />
-        <Route path="Aboutus" element={<Aboutus />} />
+      
+        <Route path="/EmptyCart" element={<EmptyCart />} />
+        <Route path="/FullCart" element={<FullCart />} />
+        <Route path="/Step01" element={<Step01 />} />
+        <Route path="/Step02" element={<Step02 />} />
+        <Route path="/Step03" element={<Step03 />} />
+        <Route path="/Step04" element={<Step04 />} />
+        <Route path="/Aboutus" element={<Aboutus />} />
       </Routes>
-    </BrowserRouter>
-  );
-}
 
-export default App;
+     </div>  
+  )
+
+} 
