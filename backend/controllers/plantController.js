d0718b9d9f96ea8f9aaec2875372f1a8cf03c99a
Pramodya R.K.K.M <minalipramodya9@gmail.com>
2024-08-28 16:05:17 +0530

    up plantcontroller

--- backend/controllers/plantController.js
+++ backend/controllers/plantController.js
@@ -1,11 +1,10 @@
 import PlantModel from "../models/plantModel.js";
-import fs from "fs";
 
+// Function to handle adding a plant
 const addPlant = async (req, res) => {
+    const image_filename = req.file ? `${req.file.filename}` : null;
 
-    let image_filename = `${req.file.filename}`;
-
-    const plant = new PlantModel({ 
+    const plant = new PlantModel({
         name: req.body.name,
         scientificName: req.body.scientificName,
         description: req.body.description,
@@ -13,28 +12,26 @@ const addPlant = async (req, res) => {
         price: req.body.price,
         countInStock: req.body.countInStock,
         category: req.body.category,
-    })
+    });
 
     try {
         await plant.save();
-        res.json({success: true, message: "Plant Added Successfully"})
+        res.json({ success: true, message: "Plant Added Successfully" });
+    } catch (error) {
+        console.error('Error adding plant:', error);
+        res.status(500).json({ success: false, message: "Plant Addition Failed" });
     }
-    catch (error) {
-        console.log(error)
-        res.json({success: false, message: "Plant Addition Failed"})
-       
-    }
-}
-// all plant list
+};
+
+// Function to handle listing all plants
 const listPlant = async (req, res) => {
     try {
-        const plants = await PlantModel.find();
-        res.json({success: true, date:plants})
+        const plants = await PlantModel.find({});
+        res.json({ success: true, data: plants });
+    } catch (error) {
+        console.error('Error fetching plant list:', error);
+        res.status(500).json({ success: false, message: "Failed to fetch plant list" });
     }
-    catch (error) {
-        console.log(error)
-        res.json({success: false, message: "Failed to fetch plant list"})
-    }
-}
+};
 
-export{addPlant, listPlant}
+export { addPlant, listPlant };
